e1c54b4b9eb112c6be5b77596620e3e2c1535ce1
Sankesh Pathak <sankesh.pathak@mastercard.com>
2024-12-14 13:22:24 +0530

    logging implementation

---
+++ UserAuthenticationService/src/main/java/com/service/userauthentication/dtos/SendNotificationMessageDTO.java
@@ -0,0 +1,16 @@
+package com.service.userauthentication.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SendNotificationMessageDTO {
+    private String to;
+    private String from;
+    private String subject;
+    private String body;
+    private Long number;
+}
